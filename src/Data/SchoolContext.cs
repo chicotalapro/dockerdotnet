ee9d022d1429e978faaf0a274c03192aa6d7dec2
HungHQ <chicotalapro@gmail.com>
2023-07-02 21:29:26 +0700

    update

---
+++ src/Data/SchoolContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace myWebApp.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
+        public DbSet<Model.Students>? Students { get; set; }
+    }
+}
